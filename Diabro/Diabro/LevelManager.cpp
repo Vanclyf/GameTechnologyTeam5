092dfbf9ae3c85b8132425b209286354f5256a63
Jasper Meier <Jasper.meier93@gmail.com>
2017-03-21 23:55:16 +0100

    Edit: makeLists file updated, Removed magic number 1

--- Diabro/Diabro/LevelManager.cpp
+++ Diabro/Diabro/LevelManager.cpp
@@ -33,7 +33,7 @@ void LevelManager::initialize()
 	npcSpawner = new CharacterSpawner<Npc>(npcSpawnerNode, 3, Ogre::Vector3(-1000, 25, -1000));
 
 	Ogre::SceneNode* enemySpawnerNode = _levelNode->createChildSceneNode("enemySpawn");
-	enemySpawner = new CharacterSpawner<BasicEnemy>(enemySpawnerNode, 3, Ogre::Vector3(1000, 1, 1000));
+	enemySpawner = new CharacterSpawner<BasicEnemy>(enemySpawnerNode, 3, Ogre::Vector3(1000, 0, 1000));
 
 	// ground 
 	createGroundMesh();
