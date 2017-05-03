7615024f4163f46ea906074152e10f070eb43775
Jasper <jasper.meier93@gmail.com>
2017-05-03 22:18:08 +0200

    Added: first test files

--- Diabro/Diabro/UnitTest/UnitTest/LevelGenerationTest.cpp
+++ Diabro/Diabro/UnitTest/UnitTest/LevelGenerationTest.cpp
@@ -5,8 +5,28 @@
 #include "City.h"
 
 TEST_CASE("level generator : city spaces", "[level-generation][cities]") {
-	Zone zone = Zone(26, 26, 5, 5, 5, 100);
+	
 	SECTION("city sizes") {
-		
+		GIVEN("a new zone is created")
+		{
+			Zone zone = Zone(9, 9, 3, 3, 2, 100);
+			WHEN("maximum city size is 3")
+			{
+				THEN("no rooms exist with a size bigger than 3")
+				{
+					REQUIRE(zone.cities.size() > 0);
+					int maxX = 0;
+					int maxZ = 0;
+					for (int i = 0; i < zone.cities.size(); ++i)
+					{
+						maxX = (maxX < zone.cities[i].width) ? zone.cities[i].width : maxX;
+						maxZ = (maxZ < zone.cities[i].width) ? zone.cities[i].width : maxZ;
+					}
+					REQUIRE(maxX <= 3);
+					REQUIRE(maxZ <= 3);
+					
+				}
+			}
+		}
 	}
 }
