b310b47c9264b34b2158eb0e5a0d488530164aa5
DESKTOP-NEHV21A\Glenn <glenn.hoff@hva.nl>
2017-05-11 23:58:25 +0200

    added: bullet physics engine integration with an example in LevelManager. for this to work it is needed to download the bullet library from google drive @ GT Team 5/Physics Engine. add a local variable "BULLET_HOME" to link the one in visual studio. it must point to the directory of the physics engine

--- Diabro/Diabro/LevelManager.cpp
+++ Diabro/Diabro/LevelManager.cpp
@@ -1,5 +1,6 @@
 #include "GameManager.h"
 #include "LevelManager.h"
+#include <btBulletDynamicsCommon.h>
 
 /// <summary>
 /// Initializes a new instance of the <see cref="LevelManager" /> class.
@@ -16,6 +17,77 @@ playerScript(0), _levelNode(0), _camNode(0), npcSpawner(0)
 /// </summary>
 void LevelManager::initialize()
 {
+
+	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+
+	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+
+	btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
+
+	btCollisionShape* fallShape = new btSphereShape(1);
+
+
+	btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+	btRigidBody::btRigidBodyConstructionInfo
+		groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+	btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+	dynamicsWorld->addRigidBody(groundRigidBody);
+
+
+	btDefaultMotionState* fallMotionState =
+		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+	btScalar mass = 1;
+	btVector3 fallInertia(0, 0, 0);
+	fallShape->calculateLocalInertia(mass, fallInertia);
+	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+	btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+	dynamicsWorld->addRigidBody(fallRigidBody);
+
+
+	for (int i = 0; i < 300; i++) {
+		dynamicsWorld->stepSimulation(1 / 60.f, 10);
+
+		btTransform trans;
+		fallRigidBody->getMotionState()->getWorldTransform(trans);
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+		FILE* fp;
+		freopen_s(&fp, "CONOUT$", "w", stdout);
+		//printf("Error opening text file, file maybe corrupt or unreachable");
+		std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+		fclose(fp);
+#endif
+	}
+
+	dynamicsWorld->removeRigidBody(fallRigidBody);
+	delete fallRigidBody->getMotionState();
+	delete fallRigidBody;
+
+	dynamicsWorld->removeRigidBody(groundRigidBody);
+	delete groundRigidBody->getMotionState();
+	delete groundRigidBody;
+
+
+	delete fallShape;
+
+	delete groundShape;
+
+
+	delete dynamicsWorld;
+	delete solver;
+	delete collisionConfiguration;
+	delete dispatcher;
+	delete broadphase;
+
+
 	// create level node, the root node for everything in the level
 	_levelNode = GameManager::getSingletonPtr()->getSceneManager()->getRootSceneNode()->createChildSceneNode("LevelNode");
 
@@ -141,6 +213,8 @@ void LevelManager::update(const Ogre::FrameEvent& pFE)
 	{
 		_hostileNpcScripts[i]->update(pFE.timeSinceLastFrame);
 	}
+
+
 }
 
 /// <summary>
