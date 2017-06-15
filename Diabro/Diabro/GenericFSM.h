e3cb5e0890b06afc9b13eb4bd3924efa7f6545b8
frederick <f.ph.vandermeulen@gmail.com>
2017-06-15 10:34:58 +0200

    Code cleanup

--- Diabro/Diabro/GenericFSM.h
+++ Diabro/Diabro/GenericFSM.h
@@ -2,6 +2,13 @@
 #define GENERIC_FINITE_STATE_MACHINE_H
 #include "State.h"
 
+/// <summary>
+/// The Generic Finite state machine class consits of an begin state method, end state method and the current state.
+/// Always begin with the beginstate method to set your start state. if not set, an empty state will be set.
+/// To switch between states, set in the code into what state the FSM must be changed.
+/// To end the state machine, set the end state machine somewhere in your code.
+/// This way, the State machine has a lot of freedom in setting and handling other states
+/// </summary>
 class GenericFSM
 {
 public:
@@ -13,8 +20,6 @@ public:
 	virtual void endState(State* pState);
 
 	void setState(State* pState);
-
-	virtual void update();
 
 	State* getCurrentState() { return _currentState; }
 
--- Diabro/Diabro/State.h
+++ Diabro/Diabro/State.h
@@ -2,6 +2,13 @@
 #define STATE_H_
 #include <OgrePrerequisites.h>
 
+/// <summary>
+/// The stae class consits of an begin action, update action and end action.
+/// The begin action is always called when the FSM activates an state object.
+/// Whenever you want to update this state(setting new values or something like that), then call the update action in the FSM
+/// When an state is ended, the endaction is always called.
+/// With this setup, it's possible to create more natural behaviour, for example, with an pathfinding you can set the pathfinding better
+/// </summary>
 class State
 {
 public:
